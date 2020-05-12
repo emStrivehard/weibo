0d6248b191f8aeab924a088f8175e3d365d404d0
zheng <373578339@qq.com>
2020-05-12 16:21:53 +0800

    用户个人页面

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="">
+    <a href="#">刻意练习,每日精进</a>
+    <div class="float-right">
+        <a href="{{ route('about')}}">关于我们</a>
+    </div>
+</footer>
