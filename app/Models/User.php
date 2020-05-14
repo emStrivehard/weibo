e9d5841afff53f4f0a025d0f5730e5e3c74efa7a
zheng <373578339@qq.com>
2020-05-14 16:03:25 +0800

    用户注册激活

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -51,5 +51,13 @@ class User extends Authenticatable
     protected function serializeDate(\DateTimeInterface $date)
     {
         return $date->format($this->getDateFormat());
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($user) {
+           $user->activation_token = Str::random(10);
+        });
     }
 }
